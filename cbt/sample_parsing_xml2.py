df9df8771fb4947a9489c4670e4957e03d4d188f
sulthan <muhammad.ghazali@global.ntt>
2021-07-30 21:56:05 +0700

    add parser xml

---
+++ cbt/sample_parsing_xml2.py
@@ -0,0 +1,19 @@
+from lxml import etree as ET
+
+#Get the XML file data
+stream = open('sample_parsing_xml.xml', 'r')
+
+#Parse the data into an ElementTree object
+xml = ET.parse(stream)
+
+#Get the 'root' Element object from the ElementTree
+root = xml.getroot()
+
+#Iterate through each child of the root Element
+for e in root:
+	#Print the stringfied version of the element
+	print(ET.tostring(e))
+	print("")
+
+	#Print the "id" attribute of the Element object
+	print(e.get("id"))
